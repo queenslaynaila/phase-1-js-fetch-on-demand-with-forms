c414fcbb566be44c46c3ac55456674b33915d04b
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-24 01:36:50 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const userInput = document.getElementById("searchByID");
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${userInput.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
